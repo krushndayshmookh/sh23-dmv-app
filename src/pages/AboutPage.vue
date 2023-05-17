12f944158904fd345dbbb1f8a05c7f0aedeb3ea6
Akanksha <akanksha.bharambe1609@gmail.com>
2023-05-17 15:14:26 +0530

    changes in title

--- src/pages/AboutPage.vue
+++ src/pages/AboutPage.vue
@@ -2,9 +2,7 @@
   <q-page class="q-pa-md">
     <div class="row q-col-gutter-md">
       <div class="col-12">
-        <div class="text-h4 text-center text-teal">
-          About us - Team SaaS CreatoR
-        </div>
+        <div class="text-h4 text-center text-teal">Team SaaS CreatoR</div>
       </div>
 
       <div class="col-12 text-center">
@@ -13,6 +11,22 @@
             Made with love within 36 hours!
           </div>
         </q-img>
+      </div>
+      <div class="col-12 justify-center items-center">
+        <div class="text-h6 text-center text-cyan-9 text-weight-bold">
+          Team Members
+        </div>
+
+        <div
+          class="q-pa-md text-body1 text-center"
+          style="max-width: 350px; margin-left: auto; margin-right: auto"
+        >
+          <q-list dense bordered padding class="rounded-borders">
+            <q-item clickable v-ripple v-for="person in people" :key="person">
+              <q-item-section> {{ person }} </q-item-section>
+            </q-item>
+          </q-list>
+        </div>
       </div>
     </div>
   </q-page>
@@ -27,6 +41,19 @@ export default defineComponent({
   setup() {
     const generalStore = useGeneralStore()
     generalStore.showBack = true
+
+    let people = [
+      'Akanksha Bharambe',
+      'Akshada Karade',
+      'Ganesh Neema',
+      'Krushn Dayshmookh',
+      'Nikhil Patil',
+      'Omkar Kale',
+    ]
+
+    return {
+      people,
+    }
   },
 })
 </script>
@@ -34,5 +61,11 @@ export default defineComponent({
 <style lang="scss">
 .team-img {
   max-width: 50vw;
+  border-radius: 8px;
+  transition: all 0.18s ease-in-out;
+
+  &:hover {
+    transform: scale(1.05);
+  }
 }
 </style>
